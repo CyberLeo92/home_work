307c664125e720d247ed5cbf41a18e8d61981bea
Leonid_Gulaev <cybernooblo92@mail.ru>
2025-04-30 09:17:21 +0300

    доработана функция "filter_by_state" в части обновления Docstring и аннотации типов

--- scr/processing.py
+++ scr/processing.py
@@ -1,6 +1,9 @@
-def filter_by_state(list_info: list[dict], state: str = 'EXECUTED') -> list[dict]:
+from typing import Any
+
+def filter_by_state(list_info: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
     """
-    Функция сортировки по ключу 'state'
+    Функция для сортировки по ключу 'state'.
+    Она принимает список словарей и возвращает новый список словарей, согласно ключа 'state'.
     """
     new_list = []
     for status in list_info:
