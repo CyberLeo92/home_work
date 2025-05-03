f8b5dcfe998439bf2f04a633d53637c5a6f2501a
Leonid_Gulaev <cybernooblo92@mail.ru>
2025-05-03 21:24:11 +0300

    в функции "get_mask_card_number" переименовал  параметр с "car_number" в "card_numbers"

--- scr/masks.py
+++ scr/masks.py
@@ -1,10 +1,10 @@
-def get_mask_card_number(car_number: str) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """
     Функция принимает на вход номер карты
     в виде числа и возвращает маску номера по правилу
     XXXX XX** **** XXXX
     """
-    return f"{car_number[:4]} {car_number[4:6]}** **** {car_number[12:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: str) -> str:
