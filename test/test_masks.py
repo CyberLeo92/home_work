3f811632b0b80b71489054416d1b44d1fd034728
Leonid_Gulaev <cybernooblo92@mail.ru>
2025-05-18 18:48:01 +0300

    Оптимизирован стиль и формат через "black" и "isort"

--- test/test_masks.py
+++ test/test_masks.py
@@ -3,10 +3,13 @@ import pytest
 from scr.masks import get_mask_account, get_mask_card_number
 
 
-@pytest.mark.parametrize("card_number, expected_card", [
-    ("7000792289606361", "7000 79** **** 6361"),
-    ("4484871238190252", "4484 87** **** 0252"),
-])
+@pytest.mark.parametrize(
+    "card_number, expected_card",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("4484871238190252", "4484 87** **** 0252"),
+    ],
+)
 def test_get_mask_card_number(card_number: str, expected_card: str) -> None:
     assert get_mask_card_number(card_number) == expected_card
 
@@ -17,10 +20,13 @@ def test_get_mask_incorrect_card(incorrect_card: str) -> None:
     assert get_mask_card_number("") == incorrect_card
 
 
-@pytest.mark.parametrize("account_number, expected_acc", [
-    ("73654108430135874305", "**4305"),
-    ("18238123718932913455", "**3455"),
-])
+@pytest.mark.parametrize(
+    "account_number, expected_acc",
+    [
+        ("73654108430135874305", "**4305"),
+        ("18238123718932913455", "**3455"),
+    ],
+)
 def test_get_mask_account(account_number: str, expected_acc: str) -> None:
     assert get_mask_account(account_number) == expected_acc
 
--- test/test_processing.py
+++ test/test_processing.py
@@ -6,24 +6,27 @@ from scr.processing import filter_by_state, sort_by_date
 
 
 def test_filter_by_executed(filter_state: list[dict[str, Any]]) -> None:
-    assert filter_by_state(filter_state, state='EXECUTED') == [
-        {'id': 816976472, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 765799246, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    assert filter_by_state(filter_state, state="EXECUTED") == [
+        {"id": 816976472, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 765799246, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
 
 def test_filter_by_canceled(filter_state: list[dict[str, Any]]) -> None:
-    assert filter_by_state(filter_state, state='CANCELED') == [
-        {'id': 123812831, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 809385983, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    assert filter_by_state(filter_state, state="CANCELED") == [
+        {"id": 123812831, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 809385983, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
 
-@pytest.mark.parametrize("received_data, expected_data", [
-    ([{'date': '2019-05-22'}, {'date': '2021-08-06'}], [{'date': '2021-08-06'}, {'date': '2019-05-22'}]),
-    ([{'date': '2022-09-01'}, {'date': '2021-09-16'}], [{'date': '2022-09-01'}, {'date': '2021-09-16'}]),
-    ([{'date': '2020-07-01'}, {'date': '2020-07-01'}], [{'date': '2020-07-01'}, {'date': '2020-07-01'}])
-])
+@pytest.mark.parametrize(
+    "received_data, expected_data",
+    [
+        ([{"date": "2019-05-22"}, {"date": "2021-08-06"}], [{"date": "2021-08-06"}, {"date": "2019-05-22"}]),
+        ([{"date": "2022-09-01"}, {"date": "2021-09-16"}], [{"date": "2022-09-01"}, {"date": "2021-09-16"}]),
+        ([{"date": "2020-07-01"}, {"date": "2020-07-01"}], [{"date": "2020-07-01"}, {"date": "2020-07-01"}]),
+    ],
+)
 def test_sort_by_date(received_data: list[dict], expected_data: list[dict]) -> None:
     assert sort_by_date(received_data) == expected_data
 
